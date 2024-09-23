4172d76d37f1ee111d40c828556e8e7fe4e87f03
alimardon <aboqijonov12@gmail.com>
2024-09-23 10:58:33 +0500

    first commit

--- ci_cd/task_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('task/', TaskList.as_view(), name='task-list'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-]
